b9684358b3c54a1cf19b6aa5c43c4de74463ebe6
Lukas Mazan <115067813+Leg3ndYe@users.noreply.github.com>
2023-03-28 21:01:23 +0200

    First Commit

--- PrvaAplikacija/Program.cs
+++ PrvaAplikacija/Program.cs
@@ -11,7 +11,9 @@ namespace PrvaAplikacija
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+
+            Console.WriteLine("My name is");
+
             Console.ReadKey();
         }
     }
